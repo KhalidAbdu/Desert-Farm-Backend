fe2c6f7c59af2114ee4f317aca7d60fc4af6dc3c
Khaled Abdou <khalid.elsehsah@gmail.com>
2023-05-20 20:07:45 +0800

    Adding Specific Product Route

--- app.js
+++ app.js
@@ -1,24 +1,17 @@
-// ℹ️ Gets access to environment variables/settings
-// https://www.npmjs.com/package/dotenv
-require("dotenv").config();
+require('dotenv').config(); // access to environment variables/settings
 
-// ℹ️ Connects to the database
-require("./db");
+require('./db'); // Connects to the database
 
-// Handles http requests (express is node js framework)
-// https://www.npmjs.com/package/express
-const express = require("express");
+const express = require('express'); // Handles http requests
 
 const app = express();
 
-// ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
-require("./config")(app);
+require('./config')(app); // It runs most pieces of middleware
 
 // 👇 Start handling routes here
-const indexRoutes = require("./routes/index.routes");
-app.use("/api", indexRoutes);
+const indexRoutes = require('./routes/index.routes');
+app.use('/api/products', indexRoutes);
 
-// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
-require("./error-handling")(app);
+require('./error-handling')(app); // To handle errors
 
 module.exports = app;
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,17 @@
-const router = require("express").Router();
+const router = require('express').Router();
+const data = require('../data.js');
 
-router.get("/", (req, res, next) => {
-  res.json("All good in here");
+router.get('/', (req, res, next) => {
+  res.json(data.products);
+});
+router.get('/slug/:slug', (req, res, next) => {
+  const { slug } = req.params;
+  const product = data.products.find((oneProduct) => oneProduct.slug === slug);
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).send({ message: 'Product Not Found' });
+  }
 });
 
 module.exports = router;
