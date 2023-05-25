43243e7c76a7a0c91cc3f7b9cfb3418222ef35b9
Khaled Abdou <khalid.elsehsah@gmail.com>
2023-05-25 11:07:11 +0800

    Profile Page

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const User = require('../models/User.model');
 const bcrypt = require('bcryptjs');
 const router = express.Router();
-const { generateToken } = require('../utils.js');
+const { generateToken, isAuth } = require('../utils.js');
 
 router.post('/signin', async (req, res) => {
   try {
@@ -29,7 +29,7 @@ router.post('/signup', async (req, res) => {
     const newUser = new User({
       name: req.body.name,
       email: req.body.email,
-      password: bcrypt.hashSync(req.body.password),
+      password: bcrypt.hashSync(req.body.password, 8),
     });
     const user = await newUser.save();
     res.send({
@@ -41,6 +41,32 @@ router.post('/signup', async (req, res) => {
     });
   } catch (error) {
     console.log(error);
+  }
+});
+
+router.put('/profile', isAuth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+    if (user) {
+      user.name = req.body.name;
+      user.email = req.body.email;
+      if (req.body.password) {
+        user.password = bcrypt.hashSync(req.body.password, 8);
+      }
+      const newUser = await user.save();
+      res.send({
+        _id: newUser._id,
+        name: newUser.name,
+        email: newUser.email,
+        isAdmin: newUser.isAdmin,
+        token: generateToken(newUser),
+      });
+    } else {
+      res.status(404).send({ message: 'User Not Found' });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error });
   }
 });
 module.exports = router;
